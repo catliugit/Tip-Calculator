ce731705c08b5301fbbbd550b24218e8e668d558
Cathy Liu <36806135+catliugit@users.noreply.github.com>
2025-01-10 13:06:35 -1000

    Create a bill with tip calculation
    tip/100 * bill + bill

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,3 +2,4 @@ print ("Welcome to the tip calculator")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15"))
 people = int(input("How many people to split the bill?"))
+bill_with_tip = tip / 100 * bill + bill
